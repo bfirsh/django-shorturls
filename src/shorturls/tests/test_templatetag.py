044c6f6fbef4807d0babae6e6e1905f68ba1d86f
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-03-15 17:56:14 -0400

    whitespace

--- src/shorturls/tests/test_templatetag.py
+++ src/shorturls/tests/test_templatetag.py
@@ -15,7 +15,7 @@ class TemplateTagTestCase(TestCase):
             'A': 'shorturls.animal',
             'V': 'shorturls.vegetable',
         }
-        
+
     def tearDown(self):
         if self.old_shorten is not None:
             settings.SHORTEN_MODELS = self.old_shorten
@@ -24,11 +24,11 @@ class TemplateTagTestCase(TestCase):
 
     def render(self, t, **c):
         return template.Template('{% load shorturl %}'+t).render(c)
-        
+
     def test_shorturl(self):
         r = self.render('{% shorturl a %}', a=Animal.objects.get(id=12345))
         self.assertEqual(r, '/ADNH')
-        
+
     def test_bad_context(self):
         r = self.render('{% shorturl a %}')
         self.assertEqual(r, '')
@@ -36,13 +36,12 @@ class TemplateTagTestCase(TestCase):
     def test_no_prefix(self):
         r = self.render('{% shorturl m %}', m=Mineral.objects.all()[0])
         self.assertEqual(r, '')
-        
+
     def test_short_base_url(self):
         settings.SHORT_BASE_URL = 'http://example.com/'
         r = self.render('{% shorturl a %}', a=Animal.objects.get(id=12345))
         self.assertEqual(r, 'http://example.com/ADNH')
-        
+
     def test_revcanonical(self):
         r = self.render('{% revcanonical a %}', a=Animal.objects.get(id=12345))
         self.assertEqual(r, '<link rev="canonical" href="/ADNH">')
-        
