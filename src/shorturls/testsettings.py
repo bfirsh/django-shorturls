0158a679083f030980e5463b2e7af906423cf6b0
Ben Firshman <ben@firshman.co.uk>
2014-09-20 18:43:26 -0700

    Add support for Django 1.7, remove 1.2 and 1.3

--- src/shorturls/testsettings.py
+++ src/shorturls/testsettings.py
@@ -1,6 +1,7 @@
 import os
 
 DEBUG = TEMPLATE_DEBUG = True
+SECRET_KEY = '123'
 
 # For Pre-Django 1.3
 DATABASE_ENGINE = 'sqlite3'
@@ -15,4 +16,4 @@ DATABASES = {
 
 INSTALLED_APPS = ['shorturls']
 ROOT_URLCONF = ['shorturls.urls']
-TEMPLATE_DIRS = os.path.join(os.path.dirname(__file__), 'tests', 'templates')
+TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), 'tests', 'templates'),)
